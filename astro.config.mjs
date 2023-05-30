8e3856c1445df760a03b0c567cb6d24b47b25594
Marco Rehmer <mrehmer-services@mailbox.org>
2023-05-30 10:53:24 +0200

    change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import {defineConfig} from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     integrations: [],
-    experimental: {assets: true}
+    experimental: {assets: true},
+    base: '/marco-rehmer'
 });
