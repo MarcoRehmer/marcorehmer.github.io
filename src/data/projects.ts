0b89db13f392c51ed72ff97639e2c052fcd57813
Marco Rehmer <mrehmer-services@mailbox.org>
2023-08-03 21:59:47 +0200

    add project descriptions for squirrel news and portfolio project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,8 +20,21 @@ export const projects: ReadonlyArray<Project> = [
     projectImageUrl: "/assets/images/projects/portfolio_thumbnail.png",
     projectImageAltText: "Portfolio Project Image",
     tags: ["Astro", "Typescript"],
-    shortDescription: "Lorem Ipsum",
-    fullText: "Foo Bar Full Text",
+    shortDescription:
+        `<p>Ein Portfolio stellt in der heutigen Zeit die Visitenkarte eines Menschen dar. </p>
+         <p>In meinem Portfolio ist eine Beschreibung zu meiner Person zu finden, 
+         stelle ich meine Open Source Projekte vor und präsentiere eine chronologische Auflistung meiner vergangenen 
+         Business-Projekte und Arbeitgeber.</p>`,
+    fullText:
+        `<p>Für meine Arbeit als Freelancer ist es wichtig, meinen Kund:innen einen Einblick in meine Fähigkeiten 
+        und erfolgreich abgeschlossenen Projekten zu bieten.</p>
+        <p>Da ich von Natur aus neugierig bin, habe ich mich hier an ein neues Framework gewagt um mein Portfolio 
+        zu erstellen: Die Rede ist von <a href='https://astro.build/'>Astro <i class=\"fa-solid fa-arrow-up-right-from-square\"></i></a>. 
+        Astro bietet die Möglichkeit performante statische 
+        Seiten im Komponenten-Baukastensystem zu entwickeln und gleichzeitig die Vorzüge der Sprache Typescript zu genießen.</p>
+        <p>Dadurch ist das Portfolio auch gleichzeitig ein \"Spielplatz\", um neue Techniken auszuprobieren. Eines der 
+        kommenden Features wird die Anbindung an ein sogenanntes \"Headless CMS\" sein um auch einen kleinen Blog mit 
+        wissenswerten Beiträgen und Storys aus meinem Entwickler-Alltag zu veröffentlichen.</p>`,
   },
   {
     title: "Squirrel News App",
@@ -32,9 +45,9 @@ export const projects: ReadonlyArray<Project> = [
     projectImageAltText: "Squirrel News App Project Image",
     tags: ["Ionic", "React", "Firebase"],
     shortDescription:
-      "Squirrel News ist ein kuratierter, konstruktiver Nachrichtendienst. " +
-      "Es werden dei wichtigsten lösungsorientierten Beiträge aus den internationalen Medien gesammelt und in kompakten, " +
-      "kostenlosen Ausgaben gebündelt.",
+      "<p>Squirrel News ist ein kuratierter, konstruktiver Nachrichtendienst.</p> " +
+        "<p>Es werden die wichtigsten lösungsorientierten Beiträge aus den internationalen Medien gesammelt und in kompakten, " +
+        "kostenlosen Ausgaben gebündelt.</p>",
     fullText:
       "<p>Um alle Nutzer:innen anzusprechen, wurde die Squirrel News App mit dem Ionic Framework und React entwickelt. " +
       "Dadurch ist es möglich, sie sowohl für iOS und Android bereitzustellen.</p>" +
