edd80c5ec9ee796a2b8211da30ab6afa64f82600
Marco Rehmer <mrehmer-services@mailbox.org>
2024-01-16 10:55:26 +0100

    remove old portfolio

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import {defineConfig} from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    integrations: [],
-    experimental: {assets: true},
-    site: 'https://marcorehmer.github.io',
-});
