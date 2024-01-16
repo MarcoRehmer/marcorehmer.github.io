07978767d2ba4398014451012e8ff28c988bad16
Marco Rehmer <mrehmer-services@mailbox.org>
2024-01-16 11:15:55 +0100

    finish layout and spacings

--- src/app/home/components/projects-gallery/projects-gallery.component.ts
+++ src/app/home/components/projects-gallery/projects-gallery.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-projects-gallery',
-  templateUrl: 'projects-gallery.component.html',
-  styleUrl: 'projects-gallery.component.scss',
+  selector: "app-projects-gallery",
+  templateUrl: "projects-gallery.component.html",
+  styleUrl: "projects-gallery.component.scss",
 })
 export class ProjectsGalleryComponent {}
